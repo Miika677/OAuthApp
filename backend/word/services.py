e20451f26306a1e92995c9b215fc335bce3e5820
Miika677 <128377114+Miika677@users.noreply.github.com>
2026-06-15 23:51:39 +0300

    word refactor: moved transaction boundary from service layer to route layer

--- backend/word/services.py
+++ backend/word/services.py
@@ -17,12 +17,10 @@ def create_word_service(input : str, id_from_token : int, db: Session):
         exists_word.votes_total += 1
         exists_word.latest_vote = datetime.now(timezone.utc)
         db.add(Vote(word_id=exists_word.id, user_id=id_from_token))
-        db.commit()
 
     else:
         new_word = Word(word = input, latest_vote = func.now())
         db.add(new_word)
         db.flush()
         db.add(Vote(word_id=new_word.id, user_id=id_from_token))
-        db.commit()
 
