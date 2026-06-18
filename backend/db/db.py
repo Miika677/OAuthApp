cf6cf5af9b8209e012954e31a95270f65cfdcd2c
Miika677 <128377114+Miika677@users.noreply.github.com>
2026-06-18 03:06:51 +0300

    ci: correct db port handling

--- backend/db/db.py
+++ backend/db/db.py
@@ -10,7 +10,7 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
+DB_PORT = os.getenv("DB_PORT", None)
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
