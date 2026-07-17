404bfe57b660016fd1db44cf118e8f102fc5d043
Miika677 <128377114+Miika677@users.noreply.github.com>
2026-07-18 01:42:02 +0300

    modify index page info text

--- frontend/src/pages/DefaultPage.jsx
+++ frontend/src/pages/DefaultPage.jsx
@@ -1,7 +1,14 @@
 function DefaultPage() {
   return(
     <div className="d-flex flex-column align-items-center">
-      <p>Submit a word</p>
+      <h3>Welcome to Word of the Day</h3>
+      <h5>An app by <a title="Miika677 on GitHub" href="https://github.com/Miika677/">Miika677</a></h5>
+
+      <div className="mt-4 text-center">
+        <p>I hope you enjoy submitting your words of choice as much I did learning and developing this website.</p>
+        <p>Despite the fact that the site is called "Word of the Day", the displayed word updates dynamically upon every vote or submission. Get voting!</p>
+      </div>
+
     </div>
   )
 }
