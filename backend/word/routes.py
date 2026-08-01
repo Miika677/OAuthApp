95fb768415f3dd60bb6e36beeedbfb86b31ff009
Miika677 <128377114+Miika677@users.noreply.github.com>
2026-08-01 19:39:40 +0300

    Add feature: lookup other users' submissions

--- backend/word/routes.py
+++ backend/word/routes.py
@@ -1,10 +1,13 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
+from backend.constants.errors import ErrorCodes
 from backend.api.api_models import SubmissionsOut, WordIn, WordOut
 from backend.auth.services import get_current_user_id
 from backend.db.db import start_session
-from backend.word.services import create_word_service, get_submissions_service, get_word_service
+from backend.word.services import create_word_service, get_submissions_service, get_word_service, look_up_submissions_service
 
 router = APIRouter()
 
@@ -12,7 +15,10 @@ router = APIRouter()
 def get_word(db: Session = Depends(start_session)):
     word = get_word_service(db)
     if not word:
-        raise HTTPException(status_code=404, detail="No words found")
+        raise HTTPException(
+            status_code=404, 
+            detail={"code":ErrorCodes.NO_SUBMISSIONS, "message": "No submitted words"}
+        )
     
     return word
 
@@ -24,9 +30,29 @@ def create_word(input : WordIn, token_id : int = Depends(get_current_user_id), d
     return {"status" : "ok"}
 
 @router.get("/submissions", response_model= list[SubmissionsOut])
-def get_submissions(token_id : int = Depends(get_current_user_id), db: Session = Depends(start_session)):
-    submissions = get_submissions_service(token_id, db)
-    if not submissions:
-        raise HTTPException(status_code=404, detail="No submitted words")
+def get_submissions(username : Optional[str] = None, token_id : int = Depends(get_current_user_id), db: Session = Depends(start_session)):
+
+    if not username:
+        submissions = get_submissions_service(token_id, db)
+        if not submissions:
+            raise HTTPException(
+                status_code=404, 
+                detail={"code":ErrorCodes.NO_SUBMISSIONS, "message": "No submitted words"}
+            )
+          
+    if username:
+        submissions = look_up_submissions_service(username, db)
+
+        if submissions is None:
+            raise HTTPException(
+                status_code=404, 
+                detail={"code":ErrorCodes.USER_NOT_FOUND, "message": "No user found"}
+            )
+
+        if not submissions:
+            raise HTTPException(
+                status_code=404, 
+                detail={"code":ErrorCodes.NO_SUBMISSIONS, "message": "No submitted words"}
+            )
     
     return submissions
